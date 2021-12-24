cd3cdfaa0c37cacfc8ca303938ef844412ed0335
Trang521 <96570525+Trang521@users.noreply.github.com>
2021-12-24 15:51:28 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -6,8 +6,9 @@ $password = "";
 
 $db_name = "test_db";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $unname, $password, $db_name);
 
 if (!$conn) {
 	echo "Connection failed!";
 }
+?>
